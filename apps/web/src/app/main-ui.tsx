931412e541413f65bae8271367fd5dcc83083f1f
Jon Mumm <jonathanrmumm@gmail.com>
2022-08-30 06:18:50 -0700

    bottom sheet changes

--- apps/web/src/app/main-ui.tsx
+++ apps/web/src/app/main-ui.tsx
@@ -1,13 +1,24 @@
-import { useRef } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { BottomSheet, BottomSheetRef } from 'react-spring-bottom-sheet';
 import 'react-spring-bottom-sheet/dist/style.css';
 import { Lobby } from '../ui/lobby';
 
 export function MainUI() {
   const sheetRef = useRef<BottomSheetRef>(null);
+  const [open, setOpen] = useState(true);
+
+  const handleDismiss = useCallback(() => {
+    setOpen(false);
+  }, []);
 
   return (
-    <BottomSheet open ref={sheetRef}>
+    <BottomSheet
+      open={open}
+      onDismiss={handleDismiss}
+      blocking={false}
+      snapPoints={({ maxHeight }) => [maxHeight / 4, maxHeight * 0.6]}
+      ref={sheetRef}
+    >
       <Lobby />
     </BottomSheet>
   );
