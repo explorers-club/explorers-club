07e0d4f2aa07c4b10a7222feb7d622b209dc55c5
Jon Mumm <jonathanrmumm@gmail.com>
2022-09-07 05:55:50 -0700

    move config to .env.local

--- apps/party-server/src/environments/environment.ts
+++ apps/party-server/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  supabaseURL: 'http://localhost:54321',
-  supabseServiceKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSJ9.vI9obAHOGyVVKa3pD--kJlyxp-Z2zV9UUMAhKpNLAcU',
+  supabaseURL: process.env['SUPABASE_URL'],
+  supabseServiceKey: process.env['SUPABASE_KEY'],
 };
