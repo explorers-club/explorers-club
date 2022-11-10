b646a9c0ad9198b6e1a1bc0af9b3e74268cd3a84
Jon Mumm <jonathanrmumm@gmail.com>
2022-11-10 06:25:48 -0800

    selector cleanup

--- apps/web/src/routes/club/club-screen.container.tsx
+++ apps/web/src/routes/club/club-screen.container.tsx
@@ -1,34 +1,26 @@
 import { useSelector } from '@xstate/react';
-import { useActorLogger } from '../../lib/logging';
 import { Claimable } from './claimable.component';
 import { Claiming } from './claiming.container';
 import { useClubScreenActor } from './club-screen.hooks';
+import {
+  selectDoesNotExist,
+  selectHostPlayerName,
+  selectIsClaimable,
+  selectIsClaiming,
+  selectIsLoading,
+} from './club-screen.selectors';
 import { Container } from './club.styles';
 import { DoesNotExist } from './does-not-exist.component';
 import { Loading } from './loading.component';
 
 export const ClubScreen = () => {
   const clubScreenActor = useClubScreenActor();
-  useActorLogger(clubScreenActor);
-  //   const { authActor } = useContext(GlobalStateContext);
 
-  const playerName = useSelector(
-    clubScreenActor,
-    (state) => state.context.hostPlayerName
-  );
-
-  const isLoading = useSelector(clubScreenActor, (state) =>
-    state.matches('Loading')
-  );
-  const isClaimable = useSelector(clubScreenActor, (state) =>
-    state.matches('Unclaimed.Claimable')
-  );
-  const doesNotExist = useSelector(clubScreenActor, (state) =>
-    state.matches('Unclaimed.NotExist')
-  );
-  const isClaiming = useSelector(clubScreenActor, (state) =>
-    state.matches('Unclaimed.Claiming')
-  );
+  const playerName = useSelector(clubScreenActor, selectHostPlayerName);
+  const isLoading = useSelector(clubScreenActor, selectIsLoading);
+  const isClaimable = useSelector(clubScreenActor, selectIsClaimable);
+  const doesNotExist = useSelector(clubScreenActor, selectDoesNotExist);
+  const isClaiming = useSelector(clubScreenActor, selectIsClaiming);
 
   if (!playerName) {
     return <Container>error parsing URL</Container>;
--- apps/web/src/routes/club/club-screen.selectors.ts
+++ apps/web/src/routes/club/club-screen.selectors.ts
@@ -2,3 +2,15 @@ import { ClubScreenState } from './club-screen.machine';
 
 export const selectHostPlayerName = (state: ClubScreenState) =>
   state.context.hostPlayerName;
+
+export const selectIsLoading = (state: ClubScreenState) =>
+  state.matches('Loading');
+
+export const selectIsClaimable = (state: ClubScreenState) =>
+  state.matches('Unclaimed.Claimable');
+
+export const selectDoesNotExist = (state: ClubScreenState) =>
+  state.matches('Unclaimed.NotExist');
+
+export const selectIsClaiming = (state: ClubScreenState) =>
+  state.matches('Unclaimed.Claiming');
