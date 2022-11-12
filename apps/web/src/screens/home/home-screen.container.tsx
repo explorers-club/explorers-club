e94e8d476333bc0c737661ad31394bb0839d8167
Jon Mumm <jonathanrmumm@gmail.com>
2022-11-12 10:29:21 -0800

    comment

--- apps/web/src/screens/home/home-screen.container.tsx
+++ apps/web/src/screens/home/home-screen.container.tsx
@@ -8,6 +8,14 @@ import { NewUserLanding } from './new-user-landing.component';
 import { WelcomeBack } from './welcome-back.component';
 
 export function HomeScreen() {
+  // TODO Refactor this component to support
+  // more of a feed of Cards / Modules
+
+  // Atop the feed will be a NowPlaying module
+  // Below it will be a StartYourClub module
+
+  // Each module loads in dynamically and has it's
+  // own loading state
   const homeActor = useHomeScreenActor();
   useActorLogger(homeActor);
 
