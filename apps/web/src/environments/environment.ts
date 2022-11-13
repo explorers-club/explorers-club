ffd87dff623f13c9cec0eaabd6c35543e115cf2e
jonmumm <jon@explorers.club>
2022-11-12 17:13:22 -0800

    update db host

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 export const environment = {
   firebaseConfig,
   production: false,
-  supabaseUrl: 'http://jonathans-macbook-air.local:54321',
+  supabaseUrl: 'http://explorers-club.local:54321',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs',
 };
