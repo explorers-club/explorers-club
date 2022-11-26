6005592fc6c2805729a53f3c29ee77712e142e71
jonmumm <jon@explorers.club>
2022-11-26 04:30:19 -0800

    add canvas setup copied from drei storybook

---
+++ games/trivia-jam/.storybook/CanvasSetup.tsx
@@ -0,0 +1,38 @@
+// From https://github.com/pmndrs/drei/blob/master/.storybook/Setup.tsx
+import * as React from 'react';
+import { Vector3 } from 'three';
+import { Canvas, Props as CanvasProps } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+type Props = React.PropsWithChildren<
+  CanvasProps & {
+    cameraFov?: number;
+    cameraPosition?: Vector3;
+    controls?: boolean;
+    lights?: boolean;
+  }
+>;
+
+export const CanvasSetup = ({
+  children,
+  cameraFov = 75,
+  cameraPosition = new Vector3(-5, 5, 5),
+  controls = true,
+  lights = true,
+  ...restProps
+}: Props) => (
+  <Canvas
+    shadows
+    camera={{ position: cameraPosition, fov: cameraFov }}
+    {...restProps}
+  >
+    {children}
+    {lights && (
+      <>
+        <ambientLight intensity={0.8} />
+        <pointLight intensity={1} position={[0, 6, 0]} />
+      </>
+    )}
+    {controls && <OrbitControls />}
+  </Canvas>
+);
