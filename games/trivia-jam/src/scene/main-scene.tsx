72405ef0a9b8fea4087dc9b2ac4843c79bec58e4
jonmumm <jon@explorers.club>
2022-12-08 04:24:35 -0800

    add floor and skybox

--- games/trivia-jam/src/scene/main-scene.tsx
+++ games/trivia-jam/src/scene/main-scene.tsx
@@ -1,7 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Environment } from '@react-three/drei';
 import * as _ from '@react-three/fiber';
+import { DoubleSide } from 'three';
 import { Treehouse } from './treehouse.component';
 
 export function MainScene() {
-  return <Treehouse />;
+  return (
+    <>
+      <Environment preset={'forest'} background={true} />
+      <Treehouse />
+      <Floor />
+    </>
+  );
 }
+
+const Floor = () => {
+  return (
+    <mesh rotation={[Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshBasicMaterial color="#4c2921" side={DoubleSide} />
+    </mesh>
+  );
+};
