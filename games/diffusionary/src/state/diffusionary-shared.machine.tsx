93a0f6c573afd2f4935726e8ec6b7df0336df0f3
jonmumm <jon@explorers.club>
2022-12-09 15:31:49 -0800

    add some context and logic

--- games/diffusionary/src/state/diffusionary-shared.machine.tsx
+++ games/diffusionary/src/state/diffusionary-shared.machine.tsx
@@ -7,7 +7,13 @@ import {
 } from 'xstate';
 import { createModel } from 'xstate/lib/model';
 
-const diffusionarySharedModel = createModel({}, {});
+const diffusionarySharedModel = createModel(
+  {
+    currentTurnUserId: undefined as string | undefined,
+    scoresByUserId: {} as Record<number, string>,
+  },
+  {}
+);
 
 export const DiffusionarySharedEvents = diffusionarySharedModel.events;
 export type DiffusionarySharedContext = ContextFrom<
@@ -19,7 +25,47 @@ export const diffusionarySharedMachine = createMachine({
   id: 'DiffusionarySharedMachine',
   initial: 'Loading',
   states: {
-    Loading: {},
+    Loading: {
+      invoke: {
+        id: 'onAllPlayersLoaded',
+        src: 'onAllPlayersLoaded',
+        onDone: 'Playing',
+      },
+    },
+    Playing: {
+      initial: 'EnteringPrompt',
+      states: {
+        EnteringPrompt: {
+          invoke: {
+            id: 'onPlayerEnterPrompt',
+            src: 'onPlayerEnterPrompt',
+            onDone: 'Guessing',
+          },
+        },
+        Guessing: {
+          invoke: {
+            id: 'onCompleteRound',
+            src: 'onCompelteRound',
+            onDone: [
+              {
+                cond: 'isGameOver',
+                target: 'GameOver',
+              },
+              {
+                target: 'EnteringPrompt',
+              },
+            ],
+          },
+        },
+        GameOver: {
+          type: 'final' as const,
+        },
+      },
+      onDone: 'Summary',
+    },
+    Summary: {
+      type: 'final' as const,
+    },
   },
 });
 
