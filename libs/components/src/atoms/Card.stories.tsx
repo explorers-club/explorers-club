7918c16e84af0324f6d7d1f849865105321231b7
jonmumm <jon@explorers.club>
2022-12-11 06:05:09 -0800

    add card stories

---
+++ libs/components/src/atoms/Card.stories.tsx
@@ -0,0 +1,153 @@
+// Discussion on storybook reuse https://github.com/storybookjs/storybook/issues/15954
+import { Flex } from './Flex';
+import { Card } from './Card';
+import { Box } from './Box';
+import { Text } from './Text';
+import { Avatar } from './Avatar';
+import { Image } from './Image';
+import { Heading } from './Heading';
+
+export default {
+  component: Card,
+};
+
+export const Default = {
+  render: () => (
+    <Card css={{ p: '$3' }}>
+      <Heading css={{ mb: '$2' }}>Explorers Club is a Play Company</Heading>
+      <Text size="3" css={{ color: '$slate11', lineHeight: '23px' }}>
+        This is a card. Use it anywhere you can use where typically would want a
+        div/view but want some simple styling that fits with the theme. Cards
+        are interactive and have hover / press visualizations.
+      </Text>
+    </Card>
+  ),
+};
+
+export const Interactive = {
+  render: () => (
+    <Card as="a" href="#" css={{ p: '$3' }} variant="interactive">
+      <Heading css={{ mb: '$2' }}>Explorers Club is a Play Company</Heading>
+      <Text size="3" css={{ color: '$slate11', lineHeight: '23px' }}>
+        This is a card. Use it anywhere you can use where typically would want a
+        div/view but want some simple styling that fits with the theme. Cards
+        are interactive and have hover / press visualizations.
+      </Text>
+      <Flex css={{ ai: 'center', jc: 'space-between', mt: '$3' }}>
+        <Flex css={{ ai: 'center' }}>
+          <Avatar
+            size="2"
+            alt="John Smith"
+            src="https://i.pravatar.cc/400"
+            fallback="J"
+            css={{
+              mr: '$1',
+            }}
+          />
+          <Text size="2" css={{ color: '$slate11' }}>
+            Inspector T
+          </Text>
+        </Flex>
+        <Box>
+          <Text size="2" css={{ color: '$slate11' }}>
+            May 2020
+          </Text>
+        </Box>
+      </Flex>
+    </Card>
+  ),
+};
+
+export const GhostInteractive = {
+  render: () => {
+    return (
+      <Card as="a" href="#" variant="ghost" css={{ p: '$3' }}>
+        <Image
+          src="https://images.unsplash.com/photo-1453235421161-e41b42ebba05?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80"
+          css={{ br: '$1', mb: '$3' }}
+        />
+        <Heading css={{ mb: '$2' }}>Explorers Club is a Play Company</Heading>
+        <Text size="3" css={{ color: '$slate11', lineHeight: '23px' }}>
+          This is a card. Use it anywhere you can use where typically would want
+          a div/view but want some simple styling that fits with the theme.
+          Cards are interactive and have hover / press visualizations.
+        </Text>
+        <Flex css={{ ai: 'center', jc: 'space-between', mt: '$3' }}>
+          <Flex css={{ ai: 'center' }}>
+            <Avatar
+              size="2"
+              alt="John Smith"
+              src="https://i.pravatar.cc/400"
+              fallback="J"
+              css={{
+                mr: '$1',
+              }}
+            />
+            <Text size="2" css={{ color: '$slate11' }}>
+              Inspector T
+            </Text>
+          </Flex>
+          <Box>
+            <Text size="2" css={{ color: '$slate11' }}>
+              May 2020
+            </Text>
+          </Box>
+        </Flex>
+      </Card>
+    );
+  },
+};
+
+export const GraphicHeader = {
+  render: () => {
+    return (
+      <Card as="a" href="#" variant="interactive">
+        <Image
+          src="https://images.unsplash.com/photo-1453235421161-e41b42ebba05?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80"
+          css={{ btlr: '$3', btrr: '$3' }}
+        />
+        <Box css={{ p: '$3' }}>
+          <Text size="3" css={{ color: '$slate11', lineHeight: '23px' }}>
+            This is a card. Use it anywhere you can use where typically would
+            want a div/view but want some simple styling that fits with the
+            theme. Cards are interactive and have hover / press visualizations.
+          </Text>
+          <Flex css={{ ai: 'center', jc: 'space-between', mt: '$3' }}>
+            <Flex css={{ ai: 'center' }}>
+              <Avatar
+                size="2"
+                alt="John Smith"
+                src="https://i.pravatar.cc/400"
+                fallback="J"
+                css={{
+                  mr: '$1',
+                }}
+              />
+              <Text size="2" css={{ color: '$slate11' }}>
+                Inspector T
+              </Text>
+            </Flex>
+            <Box>
+              <Text size="2" css={{ color: '$slate11' }}>
+                May 2020
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Card>
+    );
+  },
+};
+
+export const Active = {
+  render: () => (
+    <Card as="button" variant="active" css={{ p: '$3' }}>
+      <Text size="3" css={{ lineHeight: '23px', fontWeight: 500 }}>
+        Default Variants
+      </Text>
+      <Text size="3" css={{ color: '$slate11', lineHeight: '23px' }}>
+        Explorers Club, Limited.
+      </Text>
+    </Card>
+  ),
+};
