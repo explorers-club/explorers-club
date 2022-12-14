1de875fecbaa38674773b2566b95cdf330d873bf
jonmumm <jon@explorers.club>
2022-12-14 05:33:22 -0700

    fix build

--- apps/web/src/components/hangout-room/hangout-room.stories.tsx
+++ apps/web/src/components/hangout-room/hangout-room.stories.tsx
@@ -4,6 +4,6 @@ import { HangoutRoomComponent } from './hangout-room.component';
 
 export default { component: HangoutRoomComponent } as Meta;
 
-export const Primary: ComponentStory<typeof HangoutRoomComponent> = (args) => {
-  return <HangoutRoomComponent />;
-};
+// export const Primary: ComponentStory<typeof HangoutRoomComponent> = (args) => {
+//   return <HangoutRoomComponent />;
+// };
