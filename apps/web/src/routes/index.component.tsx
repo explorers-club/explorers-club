8bcd552a153b4af6f2ea374c5160949690fb0baf
jonmumm <jon@explorers.club>
2022-12-24 06:50:10 -0800

    homepage styling

--- apps/web/src/routes/index.component.tsx
+++ apps/web/src/routes/index.component.tsx
@@ -1,12 +1,12 @@
-import { FC } from 'react';
-import { Flex } from '@atoms/Flex';
-import { RoomAvailable } from 'colyseus.js';
-import { Skeleton } from '@atoms/Skeleton';
-import { Card } from '@atoms/Card';
-import { Text } from '@atoms/Text';
-import { Heading } from '@atoms/Heading';
-import { ClubMetadata } from '@explorers-club/schema';
+import { Box } from '@atoms/Box';
 import { Button } from '@atoms/Button';
+import { Card } from '@atoms/Card';
+import { Flex } from '@atoms/Flex';
+import { Heading } from '@atoms/Heading';
+import { Text } from '@atoms/Text';
+import { ClubMetadata } from '@explorers-club/schema';
+import { RoomAvailable } from 'colyseus.js';
+import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 interface Props {
@@ -17,33 +17,35 @@ export const IndexComponent: FC<Props> = ({ rooms }) => {
   const navigate = useNavigate();
 
   return (
-    <Flex direction="column" gap="4" css={{ p: '$3', mb: '$4' }}>
-      <Heading>Open Rooms</Heading>
-      <Flex direction="column" gap="3">
-        {rooms.length ? (
-          rooms.map((room) => (
-            <Card
-              key={room.roomId}
-              css={{ p: '$2' }}
-              variant="interactive"
-              onClick={() => navigate(`/${room.metadata?.clubName}`)}
-            >
-              <Flex>{room.metadata?.clubName}'s Explorers Club</Flex>
-            </Card>
-          ))
-        ) : (
-          <Text>There are no open club rooms.</Text>
-        )}
-        <Button
-          color="primary"
-          size="3"
-          onClick={() => {
-            navigate('/new');
-          }}
-        >
-          Start New Room
-        </Button>
-      </Flex>
-    </Flex>
+    <Box css={{ p: '$3' }}>
+      <Card>
+        <Flex css={{ p: '$3' }} direction="column" gap="3">
+          <Heading>Open Rooms</Heading>
+          {rooms.length ? (
+            rooms.map((room) => (
+              <Card
+                key={room.roomId}
+                css={{ p: '$2' }}
+                variant="interactive"
+                onClick={() => navigate(`/${room.metadata?.clubName}`)}
+              >
+                <Flex>{room.metadata?.clubName}'s Explorers Club</Flex>
+              </Card>
+            ))
+          ) : (
+            <Text>There are no open club rooms.</Text>
+          )}
+          <Button
+            color="primary"
+            size="3"
+            onClick={() => {
+              navigate('/new');
+            }}
+          >
+            Start New Room
+          </Button>
+        </Flex>
+      </Card>
+    </Box>
   );
 };
