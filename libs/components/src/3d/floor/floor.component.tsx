325e3ef520a5f98d1f6eba8d6d273b36290e3fee
jonmumm <jon@explorers.club>
2022-12-26 13:22:07 -0800

    add infinite grid

--- libs/components/src/3d/floor/floor.component.tsx
+++ libs/components/src/3d/floor/floor.component.tsx
@@ -1,10 +1,11 @@
-import { DoubleSide } from 'three';
+import { Grid } from '@react-three/drei';
 
 export const Floor = () => {
   return (
-    <mesh position={[0, 0, -5]} rotation={[Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[30, 50]} />
-      <meshBasicMaterial color="#000" side={DoubleSide} />
-    </mesh>
+    <Grid cellColor="#444" sectionColor="#444" fadeDistance={400} fadeStrength={10} infiniteGrid={true} />
+    // <mesh position={[0, 0, -5]} rotation={[Math.PI / 2, 0, 0]}>
+    //   <planeGeometry args={[30, 50]} />
+    //   <meshBasicMaterial color="#000" side={DoubleSide} />
+    // </mesh>
   );
 };
