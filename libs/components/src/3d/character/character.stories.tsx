429ca2777a3ebaa9f77b1ace354f14a72a523def
Fravic <fravicf@gmail.com>
2022-12-30 00:12:45 -0500

    Add hook for keyboard input

--- libs/components/src/3d/character/character.stories.tsx
+++ libs/components/src/3d/character/character.stories.tsx
@@ -1,49 +1,12 @@
-import { useAnimations, useGLTF } from '@react-three/drei';
 import { Meta, Story } from '@storybook/react';
-import { useInterpret } from '@xstate/react';
-import { useEffect, useMemo, useRef } from 'react';
-import * as THREE from 'three';
 import { withCanvasSetup } from '../__stories/CanvasSetup';
-import { Character } from './character.component';
-import { CharacterActor, createCharacterMachine } from './character.machine';
-import { CharacterAnimationAction, GLTFResult } from './character.types';
+import { Character, CharacterController } from './character.component';
 
 export default {
   component: Character,
   decorators: [withCanvasSetup],
 } as Meta;
 
-const Template: Story<{ run?: (actor: CharacterActor) => void }> = ({
-  run,
-}) => {
-  const gltf = useGLTF('./assets/character.glb') as unknown as GLTFResult;
-  const group = useRef<THREE.Group>(null);
-  const { actions } = useAnimations(gltf.animations, group) as unknown as {
-    actions: CharacterAnimationAction;
-  };
-  const machine = useMemo(() => createCharacterMachine(actions), [actions]);
-  const actor = useInterpret(machine);
-  useEffect(() => {
-    run && run(actor);
-  }, [actor, run]);
-
-  return <Character gltf={gltf} group={group} />;
-};
-
-export const Idle = Template.bind({});
-
-export const Shooting = Template.bind({});
-
-Shooting.args = {
-  run: (actor) => {
-    actor.send('SHOOT');
-  },
-};
-
-export const Running = Template.bind({});
-
-Running.args = {
-  run: (actor) => {
-    actor.send({ type: 'MOVE', lat: 10, lng: 10 });
-  },
+export const ControlledCharacter: Story = () => {
+  return <CharacterController />;
 };
