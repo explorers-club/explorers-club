4e438a56d6d824ff3cc14d66823638c3623186a9
jonmumm <jon@explorers.club>
2022-12-31 08:47:59 -0800

    fix themeing

--- apps/web/src/app/app.component.tsx
+++ apps/web/src/app/app.component.tsx
@@ -3,21 +3,21 @@ import { SunsetSky } from '@3d/sky';
 import { Treehouse } from '@3d/treehouse';
 import { Box } from '@atoms/Box';
 import { Sheet, SheetContent } from '@atoms/Sheet';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@atoms/Tabs';
 import { Text } from '@atoms/Text';
 import { darkTheme } from '@explorers-club/styles';
 import {
   Environment,
   OrbitControls,
-  useContextBridge
+  useContextBridge,
 } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { FC, ReactElement, Suspense } from 'react';
 import { BottomSheet } from 'react-spring-bottom-sheet';
 import {
   defaultSnapProps,
-  SnapPointProps
+  SnapPointProps,
 } from 'react-spring-bottom-sheet/dist/types';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@atoms/Tabs';
 import { Routes } from '../routes/routes.container';
 import { ColyseusContext } from '../state/colyseus.context';
 
@@ -35,23 +35,24 @@ export const AppComponent = () => {
         snapPoints={DEFAULT_SNAP_POINTS}
         expandOnContentDrag={true}
       >
-        <Tabs defaultValue="party">
-          <TabsList>
-            <TabsTrigger value="objectives">Objectives</TabsTrigger>
-            <TabsTrigger value="map">Map</TabsTrigger>
-            <TabsTrigger value="party">Party</TabsTrigger>
-          </TabsList>
-          <TabsContent value="party">
-            <Routes />
-          </TabsContent>
-          <TabsContent value="map">
-            <Text>Map</Text>
-          </TabsContent>
-          <TabsContent value="objectives">
-            <Text>Objectives</Text>
-          </TabsContent>
-        </Tabs>
-        <Box className={darkTheme.className}></Box>
+        <Box className={darkTheme.className}>
+          <Tabs defaultValue="party">
+            <TabsList>
+              <TabsTrigger value="objectives">Objectives</TabsTrigger>
+              <TabsTrigger value="map">Map</TabsTrigger>
+              <TabsTrigger value="party">Party</TabsTrigger>
+            </TabsList>
+            <TabsContent value="party">
+              <Routes />
+            </TabsContent>
+            <TabsContent value="map">
+              <Text>Map</Text>
+            </TabsContent>
+            <TabsContent value="objectives">
+              <Text>Objectives</Text>
+            </TabsContent>
+          </Tabs>
+        </Box>
       </BottomSheet>
       <SceneContainer>
         <>
