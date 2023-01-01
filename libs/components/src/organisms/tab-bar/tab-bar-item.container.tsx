99d11a2e8d3fad0513193584a4017c7e21e04d1c
jonmumm <jon@explorers.club>
2022-12-31 16:11:09 -0800

    fix story

--- libs/components/src/organisms/tab-bar/tab-bar-item.container.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { TabBarItemComponent } from './tab-bar-item.component';
-
-export const TabBarItem = () => {
-  return <TabBarItemComponent />;
-};
--- libs/components/src/organisms/tab-bar/tab-bar-item.stories.tsx
+++ libs/components/src/organisms/tab-bar/tab-bar-item.stories.tsx
@@ -1,9 +1,28 @@
+import * as Tabs from '@radix-ui/react-tabs';
 import { ComponentStory, Meta } from '@storybook/react';
+import { useInterpret } from '@xstate/react';
 import React from 'react';
 import { TabBarItemComponent } from './tab-bar-item.component';
+import { tabBarItemMachine } from './tab-bar-item.machine';
 
 export default { component: TabBarItemComponent } as Meta;
 
+const TestComponent = () => {
+  return <div>Lobby</div>;
+};
+
 export const Primary: ComponentStory<typeof TabBarItemComponent> = (args) => {
-  return <TabBarItemComponent />;
+  const actor = useInterpret(
+    tabBarItemMachine.withContext({
+      component: <TestComponent />,
+      tab: 'Lobby',
+    })
+  );
+  return (
+    <Tabs.Root>
+      <Tabs.List>
+        <TabBarItemComponent actor={actor} />
+      </Tabs.List>
+    </Tabs.Root>
+  );
 };
