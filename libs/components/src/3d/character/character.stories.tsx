91b1bcd1feded0df6f0ccc4e917b3077c4c56100
zinx110 <tasdid110@gmail.com>
2023-01-08 22:50:23 +0600

    character customization

--- libs/components/src/3d/character/character.stories.tsx
+++ libs/components/src/3d/character/character.stories.tsx
@@ -3,7 +3,6 @@ import { Meta, Story } from '@storybook/react';
 import { withCanvasSetup } from '../__stories/CanvasSetup';
 import { Character } from './character.component';
 import { CharacterController } from './character-controller.component';
-import { OrbitControls } from '@react-three/drei';
 
 export default {
   component: Character,
