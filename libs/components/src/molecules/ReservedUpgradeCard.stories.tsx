44580a36bfa58beda6ba40829e653d24ec7d47c4
jonmumm <jon@explorers.club>
2023-02-01 06:02:00 -0800

    change default viewport

--- libs/components/src/molecules/ReservedUpgradeCard.stories.tsx
+++ libs/components/src/molecules/ReservedUpgradeCard.stories.tsx
@@ -2,8 +2,13 @@ import { ReservedUpgradeCard } from './ReservedUpgradeCard';
 
 export default {
   component: ReservedUpgradeCard,
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
 };
 
 export const Default = () => {
-  return <ReservedUpgradeCard playerName='Jambalaya22' />;
+  return <ReservedUpgradeCard playerName="Jambalaya22" />;
 };
