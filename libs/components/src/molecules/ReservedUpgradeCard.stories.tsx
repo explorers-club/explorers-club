1525322ef951f73de2220f8e26e25b270a5f6e64
jonmumm <jon@explorers.club>
2023-02-01 05:49:14 -0800

    create reserved upgrade component

---
+++ libs/components/src/molecules/ReservedUpgradeCard.stories.tsx
@@ -0,0 +1,9 @@
+import { ReservedUpgradeCard } from './ReservedUpgradeCard';
+
+export default {
+  component: ReservedUpgradeCard,
+};
+
+export const Default = () => {
+  return <ReservedUpgradeCard playerName='Jambalaya22' />;
+};
