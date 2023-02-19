23cc3e4afc3eccd42f7f22a12c857e370d7005d7
jonmumm <jon@explorers.club>
2023-02-19 07:30:45 -0800

    chat styling fixes

--- apps/web/src/tabs/game/game-tab.container.tsx
+++ apps/web/src/tabs/game/game-tab.container.tsx
@@ -1,8 +1,13 @@
+import { Box } from '@atoms/Box';
 import { useContext } from 'react';
 import { AppContext } from '../../state/app.context';
 import { GameTabComponent } from './game-tab.component';
 
 export const GameTab = () => {
   const { gameTabActor } = useContext(AppContext);
-  return <GameTabComponent actor={gameTabActor} />;
+  return (
+    <Box css={{ height: '90vh' }}>
+      <GameTabComponent actor={gameTabActor} />;
+    </Box>
+  );
 };
