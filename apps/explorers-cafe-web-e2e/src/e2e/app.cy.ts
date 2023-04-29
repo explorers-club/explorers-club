06c7d5a12e87634ab4884e0ee26f016d09ab5a38
Jonathan Mumm <jon@explorers.club>
2023-04-29 14:17:25 -0700

    add cafe web app

---
+++ apps/explorers-cafe-web-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,11 @@
+import { getGreeting } from '../support/app.po';
+
+describe('explorers-cafe-web', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    cy.login('my-email@something.com', 'myPassword');
+
+    getGreeting().contains('Welcome to Astro');
+  });
+});
